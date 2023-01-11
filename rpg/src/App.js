838d9dddecc96d37e9c6106b13d28e5901aed896
FelixDuverell <Felix.duverell@gmail.com>
2023-01-11 16:36:20 +0100

    merge problem commit

--- rpg/src/App.js
+++ rpg/src/App.js
@@ -1,18 +1,1 @@
-import { BrowserRouter, Route, Routes} from "react-router-dom";
-import Signup from "./components/Signup"
-import Usermade from "./components/Usermade"
 
-function App() {
-  return (
-    <BrowserRouter>
-        <Routes>
-          <Route path="/Usermade" element={<Usermade />}></Route>
-          <Route path="/Signup" element={<Signup />}></Route>
-        </Routes>
-    </BrowserRouter>
-  )
-}
-
-
-
-export default App;
