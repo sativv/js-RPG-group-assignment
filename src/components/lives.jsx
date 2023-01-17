4376dbc2a8a00e742efd71fcdc58f0b776965478
sativ <david.vitas@hotmail.com>
2023-01-17 23:31:43 +0100

    Merge this if you want to solve motion sickness

--- src/components/lives.jsx
+++ src/components/lives.jsx
@@ -1,21 +1,20 @@
-import {useState} from 'react';
+import { useState } from "react";
 
 const Lives = () => {
+  const [lives, setLives] = useState(3);
 
-    const [lives, setLives] = useState(3);
-
-    const removeLife = () => {
-        if (lives > 0) {
-        setLives(lives - 1);
-        }
+  const removeLife = () => {
+    if (lives > 0) {
+      setLives(lives - 1);
     }
+  };
 
-    return (
-        <div>
-            <h1>Lives left: {lives}</h1>
-            <button onClick={removeLife}>Remove life</button>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Lives left: {lives}</h1>
+      <button onClick={removeLife}>Remove life</button>
+    </div>
+  );
 };
 
 export default Lives;
